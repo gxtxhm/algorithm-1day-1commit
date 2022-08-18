f5298dd9649e5e17d36530f3b524db6b66ae6473
gxtxhm <psj73327315@gmail.com>
2022-08-18 14:07:12 +0900

    finish 같은 숫자는 싫어 - 프로그래머스

---
+++ algorithmPractice/algorithmPractice/같은_숫자는_싫어.cpp
@@ -0,0 +1,30 @@
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+    vector<int> answer;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
+    answer.push_back(arr[0]);
+    int before = arr[0];
+    for (int id : arr)
+    {
+        if (before != id)
+        {
+            answer.push_back(id);
+        }
+        before = id;
+    }
+
+    return answer;
+}
+
+int main()
+{
+
+}
