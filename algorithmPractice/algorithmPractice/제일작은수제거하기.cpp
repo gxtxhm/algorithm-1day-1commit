8692c4d46ca299097ed65de3d76daeb65d92f1e8
gxtxhm <macdollyh@naver.com>
2022-08-24 23:12:29 +0900

    finish-나누어 떨어지는 숫자 배열-프로그래머스

--- algorithmPractice/algorithmPractice/제일작은수제거하기.cpp
+++ algorithmPractice/algorithmPractice/제일작은수제거하기.cpp
@@ -1,30 +1,30 @@
-#include <string>
-#include <vector>
-#include<algorithm>
-using namespace std;
-
-vector<int> solution(vector<int> arr) {
-    vector<int> answer;
-    if (arr.size() == 1)
-    {
-        answer.push_back(-1);
-        return (answer);
-    }
-    int min = 2100000000;
-
-    for (int i : arr)
-    {
-        if (min > i)min = i;
-    }
-    for (int i : arr)
-    {
-        if (min == i)continue;
-        answer.push_back(i);
-    }
-
-    return answer;
-}
-int main()
-{
-    
-}
+//#include <string>
+//#include <vector>
+//#include<algorithm>
+//using namespace std;
+//
+//vector<int> solution(vector<int> arr) {
+//    vector<int> answer;
+//    if (arr.size() == 1)
+//    {
+//        answer.push_back(-1);
+//        return (answer);
+//    }
+//    int min = 2100000000;
+//
+//    for (int i : arr)
+//    {
+//        if (min > i)min = i;
+//    }
+//    for (int i : arr)
+//    {
+//        if (min == i)continue;
+//        answer.push_back(i);
+//    }
+//
+//    return answer;
+//}
+//int main()
+//{
+//    
+//}
