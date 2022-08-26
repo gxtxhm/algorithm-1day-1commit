e1f0c7152b08088568c04f42b3b91cb1ce968601
gxtxhm <macdollyh@naver.com>
2022-08-26 17:08:43 +0900

    finish- 신규 아이디 추천-프로그래머스

--- algorithmPractice/algorithmPractice/없는숫자더하기.cpp
+++ algorithmPractice/algorithmPractice/없는숫자더하기.cpp
@@ -1,25 +1,25 @@
-#include <string>
-#include <vector>
-#include<algorithm>
-using namespace std;
-
-int solution(vector<int> numbers) {
-    int answer = 0;
-    int a[10] = { 0, };
-    for (int i : numbers)
-    {
-        a[i] = 1;
-    }
-    for (int i = 0; i <= 9; i++)
-    {
-        if (a[i] == 0)answer += i;
-    }
-
-
-
-    return answer;
-}
-int main()
-{
-    return 0;
-}
+//#include <string>
+//#include <vector>
+//#include<algorithm>
+//using namespace std;
+//
+//int solution(vector<int> numbers) {
+//    int answer = 0;
+//    int a[10] = { 0, };
+//    for (int i : numbers)
+//    {
+//        a[i] = 1;
+//    }
+//    for (int i = 0; i <= 9; i++)
+//    {
+//        if (a[i] == 0)answer += i;
+//    }
+//
+//
+//
+//    return answer;
+//}
+//int main()
+//{
+//    return 0;
+//}
