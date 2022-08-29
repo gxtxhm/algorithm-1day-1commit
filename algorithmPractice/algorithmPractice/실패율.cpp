dc6df46e4621a6422b515a177c0300ebd5e581ed
gxtxhm <macdollyh@naver.com>
2022-08-29 18:42:51 +0900

    finish-실패율-프로그래머스

---
+++ algorithmPractice/algorithmPractice/실패율.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+bool compare(const pair<int, double>& a, const pair<int, double>& b)
+{
+    if (a.second == b.second)
+        return a.first < b.first;
+    return a.second > b.second;
+}
+
+vector<int> solution(int N, vector<int> stages) {
+    vector<int> answer;
+    vector<pair<int, double>> s;
+    int a = stages.size();
+    for (int i = 1; i <= N; i++)
+    {
+        int cnt = 0;
+        for (int j = 0; j < stages.size(); j++)
+        {
+            if (i == stages[j])cnt++;
+        }
+        // 왜 이 if코드를 넣으니까 통과지??
+		  // 아마도 double형이 0.000001 이런식으로 되어있어서 그런건가
+        if (cnt == 0)s.push_back(make_pair(i, 0));
+        else s.push_back(make_pair(i, (double)cnt / a));
+        a -= cnt;
+    }
+    sort(s.begin(), s.end(), compare);
+    for (int i = 0; i < s.size(); i++)
+    {
+        answer.push_back(s[i].first);
+    }
+    return answer;
+}
+int main()
+{
+    vector<int>a = { 2, 1, 2, 6, 2,7,7,7, 4, 3,4,1, 3 };
+    solution(6, a);
+    return 0;
+}
--- algorithmPractice/algorithmPractice/하샤드수.cpp
+++ algorithmPractice/algorithmPractice/하샤드수.cpp
@@ -1,25 +1,26 @@
-#include <string>
-#include <vector>
-#include<iostream>
-using namespace std;
-
-// 아주 쉬운 문제지만, return 부분에서 나머지 연산이 아닌 몫을 구하는 연산을 사용해서
-// 값이 나오지 않았었다. 디버깅을 하며 오류를 찾을 수 있었다.
-
-bool solution(int x) {
-    bool answer = true;
-    int num = 0;
-    int original = x;
-
-    while (x > 0)
-    {
-        num += x % 10;
-        x /= 10;
-    }
-    return ((original % num) == 0);
-}
-
-int main()
-{
-    cout << solution(10);
-}
+//#include <string>
+//#include <vector>
+//#include<iostream>
+//using namespace std;
+//
+//// 아주 쉬운 문제지만, return 부분에서 나머지 연산이 아닌 몫을 구하는 연산을 사용해서
+//// 값이 나오지 않았었다. 디버깅을 하며 오류를 찾을 수 있었다.
+//
+//bool solution(int x) {
+//    bool answer = true;
+//    int num = 0;
+//    int original = x;
+//    
+//    
+//    while (x > 0)
+//    {
+//        num += x % 10;
+//        x /= 10;
+//    }
+//    return ((original % num) == 0);
+//}
+//
+//int main()
+//{
+//    cout << solution(10);
+//}
