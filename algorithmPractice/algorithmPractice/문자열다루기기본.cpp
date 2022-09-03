eba9c1f3786ed34978b85b14ad0207306883c58c
gxtxhm <macdollyh@naver.com>
2022-09-03 20:05:08 +0900

    finish-문자열내맘대로정렬하기-프로그래머스

--- algorithmPractice/algorithmPractice/문자열다루기기본.cpp
+++ algorithmPractice/algorithmPractice/문자열다루기기본.cpp
@@ -1,23 +1,23 @@
-#include <string>
-#include <vector>
-
-using namespace std;
-
-// 숫자로만 이루어졌는지를 아스키코드를 이용해서 판단하였다.
-
-bool solution(string s) {
-    bool answer = true;
-    if (s.length() == 4 || s.length() == 6)
-    {
-        bool ch = true;
-        for (char c : s)
-        {
-            if (c >= 48 && c <= 57)
-            {
-            }
-            else return false;
-        }
-    }
-    else return false;
-    return answer;
-}
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//// 숫자로만 이루어졌는지를 아스키코드를 이용해서 판단하였다.
+//
+//bool solution(string s) {
+//    bool answer = true;
+//    if (s.length() == 4 || s.length() == 6)
+//    {
+//        bool ch = true;
+//        for (char c : s)
+//        {
+//            if (c >= 48 && c <= 57)
+//            {
+//            }
+//            else return false;
+//        }
+//    }
+//    else return false;
+//    return answer;
+//}
