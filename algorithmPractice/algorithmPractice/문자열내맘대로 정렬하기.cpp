1ce9e59445e8af9401a511e7f4a42b33fcc4ec2e
gxtxhm <macdollyh@naver.com>
2022-09-04 09:03:34 +0900

    finish-콜라츠추측-프로그래머스

--- algorithmPractice/algorithmPractice/문자열내맘대로 정렬하기.cpp
+++ algorithmPractice/algorithmPractice/문자열내맘대로 정렬하기.cpp
@@ -1,46 +1,46 @@
-#include <string>
-#include <vector>
-#include<algorithm>
-using namespace std;
-
-int num;
-
-bool compare(string a, string b)
-{
-    //위의 코드와 밑의 코드의 차이가 뭐지?
-    // 밑에 코드들은 정답, 이 코드는 오답이다.
-    // 아 마지막 else 문에 return false가 아닌 false만 적어서 오답이 나왔었다. 
-    // 허무하지만 코드를 더 잘 분석해야겠다. 
-    if (a[num] < b[num])return true;
-    else if (a[num] == b[num])return a < b;
-    else return false;
-
-    /*if (s1[idx] < s2[idx])
-        return true;
-    else if (s1[idx] == s2[idx])
-        return s1 < s2;
-    else return false;*/
-    
-    /*if (a[num] == b[num]) {
-        return a < b;
-    }
-    else {
-        return a[num] < b[num];
-    }*/
-
-}
-
-vector<string> solution(vector<string> strings, int n) {
-    vector<string> answer;
-    num = n;
-    sort(strings.begin(), strings.end(), compare);
-    answer = strings;
-    return answer;
-}
-int main()
-{
-    vector<string>a = { "zbcde", "ybcdf", "xbcdg","ybccf"};
-      
-    solution(a, 1);
-    return 0;
-}
+//#include <string>
+//#include <vector>
+//#include<algorithm>
+//using namespace std;
+//
+//int num;
+//
+//bool compare(string a, string b)
+//{
+//    //위의 코드와 밑의 코드의 차이가 뭐지?
+//    // 밑에 코드들은 정답, 이 코드는 오답이다.
+//    // 아 마지막 else 문에 return false가 아닌 false만 적어서 오답이 나왔었다. 
+//    // 허무하지만 코드를 더 잘 분석해야겠다. 
+//    if (a[num] < b[num])return true;
+//    else if (a[num] == b[num])return a < b;
+//    else  false;
+//
+//    /*if (s1[idx] < s2[idx])
+//        return true;
+//    else if (s1[idx] == s2[idx])
+//        return s1 < s2;
+//    else return false;*/
+//    
+//    /*if (a[num] == b[num]) {
+//        return a < b;
+//    }
+//    else {
+//        return a[num] < b[num];
+//    }*/
+//
+//}
+//
+//vector<string> solution(vector<string> strings, int n) {
+//    vector<string> answer;
+//    num = n;
+//    sort(strings.begin(), strings.end(), compare);
+//    answer = strings;
+//    return answer;
+//}
+//int main()
+//{
+//    vector<string>a = { "zbcde", "ybcdf", "xbcdg","ybccf"};
+//      
+//    solution(a, 1);
+//    return 0;
+//}
---
+++ algorithmPractice/algorithmPractice/콜라츠추측.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+
+// 콜라츠 추측 문제인데, 테스트케이스 1개가 오류라서 살펴보았다.
+// 이 문제의 경우 int의 범위를 넘어서기 때문에 longlong형으로 변수타입을 바꿔저장해서 계산해야한다.
+// 문제를 풀 때, 계산범위가 어느정도일지 예측하여 변수타입을 지정하는 것도 신경을 써야겠다.
+
+
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    int cnt = 0;
+    if (num == 1)return 0;
+    
+
+    while (num > 1)
+    {
+        if (cnt == 500)return -1;
+        if (num % 2 == 0)num /= 2;
+        else if (num % 2 == 1)num = (num * 3 + 1);
+        cnt++;
+    }
+
+    return cnt;
+}
+
+int main()
+{
+    solution(626331);
+    return 0;
+}
