a31b3dab5dd9dcb7d63d9bdb7921286a2eebebbc
gxtxhm <macdollyh@naver.com>
2022-09-08 23:28:50 +0900

    studying-순위검사(효율성테스트 통과실패)-프로그래머스

--- algorithmPractice/algorithmPractice/[1차]뉴스 클러스터링.cpp
+++ algorithmPractice/algorithmPractice/[1차]뉴스 클러스터링.cpp
@@ -1,151 +1,151 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <cctype>
-#include<iostream>
-
-using namespace std;
-
-// 리드미 파일 사용하는법 몰라서 일단 주석으로 씀.
-
-// 1. 소문자로 문자열 변경 2. 다중집합으로 만듦 3. 오름차순으로 정렬
-// 4. 교집합 개수 구하기 5. 합집합 개수 구하기
-
-// 교집합은 id_s1, id_s2 2개의 인덱스로 0부터 비교해나가며 
-// if s1,s2문자열이 같으면 , s1의 같은 문자열을 센다. 그리고 s2의 같은 문자열을 센다.
-// 2개 중 더 작은 개수를 iCnt(교집합 원소의 개수)에 더한다.
-// else면 s1,s2중 사전순으로 더 앞인 것의 인덱스 증가.
-
-// 합집합도 거의 동일한 방식으로 개수를 구했다. 
-
-// 다른 친구와 얘기를 해보니 훨씬 효율적으로 풀어냈다. 
-// 교집합의 개수를 구할때 교집합의 개수는 두개의 집합 중 더 원소의 개수가 적은 집합의 원소개수를 
-// 넘을 수 없다는 아이디어를 적용하여 소거법으로 풀어냈다. 
-// ex) 더 원소가 적은 집합 A, 그렇지않은집합 B일때, A의 원소를 for문으로 돌며 B에서 하나씩 소거하며 
-// 같으면 개수를 증가시켰다.
-// 그리고 합집합의 개수는 n(A) + n(B) - n(A ^ B)의 공식을 이용하여 풀어냈다. 
-
-// 코딩 전, 방법이 생각나도 한번 더 생각해보며 더 효율적인 방법을 찾으려는 노력이 필요해보인다.
-
-
-void MakeMultipleSet(vector<string>& s,string str)
-{
-    for (int i = 0; i < str.length() - 1; i++)
-    {
-        // 두글자 모두 알파벳이면
-        if (isalpha(str[i]) && isalpha(str[i + 1]))
-        {
-            string a;
-            a += str[i];
-            a += str[i + 1];
-            s.push_back(a);
-        }
-    }
-}
-
-
-int solution(string str1, string str2) {
-    int answer = 0;
-
-    vector<string> s1;
-    vector<string> s2;
-
-    // 문자열의 대문자를 소문자로
-    transform(str1.begin(), str1.end(), str1.begin(), ::tolower);
-    transform(str2.begin(), str2.end(), str2.begin(), ::tolower);
-
-    //다중집합 만들기
-    MakeMultipleSet(s1, str1);
-    MakeMultipleSet(s2, str2);
-
-    if (s1.size() == 0 && s2.size()==0)return 65536;
-
-    // 오름차순 정렬
-    sort(s1.begin(), s1.end());
-    sort(s2.begin(), s2.end());
-
-    int id_s1 = 0;
-    int id_s2 = 0;
-    int cnt1 = 0, cnt2 = 0;
-    int iCnt = 0;// 교집합 개수
-    int SCnt = 0;// 합집합 개수
-    
-    // 교집합 개수 구하기
-    while (id_s1 < s1.size() && id_s2 < s2.size())
-    {
-        // 같은 원소면
-        if (s1[id_s1] == s2[id_s2])
-        {
-            int i = id_s1 + 1;
-            while (i < s1.size()&&(s1[id_s1] == s1[i]))
-            {
-                i++;
-            }
-            cnt1 = i - id_s1;
-            i = id_s2 + 1;
-            while (i < s2.size()&&(s2[id_s2] == s2[i]))
-            {
-                i++;
-            }
-            cnt2 = i - id_s2;
-            int minCnt = min(cnt1, cnt2);
-            iCnt += minCnt;
-            id_s1+=cnt1; id_s2+=cnt2;
-        }
-        else
-        {
-            if (s1[id_s1] < s2[id_s2])id_s1++;
-            else id_s2++;
-        }
-    }
-    cout << iCnt;
-    id_s1 = 0; id_s2 = 0;
-    // 합집합 개수 구하기
-    while (id_s1 < s1.size() && id_s2 < s2.size())
-    {
-        // 같은 원소면
-        if (s1[id_s1] == s2[id_s2])
-        {
-            int i = id_s1 + 1;
-            while (i < s1.size() && (s1[id_s1] == s1[i]))i++;
-            cnt1 = i - id_s1;
-            i = id_s2 + 1;
-            while (i < s2.size() && (s2[id_s2] == s2[i]))i++;
-            cnt2 = i - id_s2;
-            int maxCnt = max(cnt1, cnt2);
-            SCnt += maxCnt;
-            id_s1+=cnt1; id_s2+=cnt2;
-        }
-        else
-        {
-            if (s1[id_s1] < s2[id_s2])id_s1++;
-            else id_s2++;
-            SCnt++;
-        }
-    }
-    // 남은 원소 
-    // s1의 원소가 남았다면
-    while (id_s1 < s1.size())
-    {
-        id_s1++; SCnt++;
-    }
-    // s2의 원소가 남았다면
-    while (id_s2 < s2.size())
-    {
-        id_s2++; SCnt++;
-    }
-    cout << iCnt;
-    cout << SCnt;
-    double similarity = (double)iCnt / SCnt;
-    answer = similarity * 65536;
-    return answer;
-}
-
-
-int main()
-{
-    string a = "E=M*C^2";
-    string b = "e=m*c^2";
-    solution(a, b);
-    return 0;
-}
+//#include <string>
+//#include <vector>
+//#include <algorithm>
+//#include <cctype>
+//#include<iostream>
+//
+//using namespace std;
+//
+//// 리드미 파일 사용하는법 몰라서 일단 주석으로 씀.
+//
+//// 1. 소문자로 문자열 변경 2. 다중집합으로 만듦 3. 오름차순으로 정렬
+//// 4. 교집합 개수 구하기 5. 합집합 개수 구하기
+//
+//// 교집합은 id_s1, id_s2 2개의 인덱스로 0부터 비교해나가며 
+//// if s1,s2문자열이 같으면 , s1의 같은 문자열을 센다. 그리고 s2의 같은 문자열을 센다.
+//// 2개 중 더 작은 개수를 iCnt(교집합 원소의 개수)에 더한다.
+//// else면 s1,s2중 사전순으로 더 앞인 것의 인덱스 증가.
+//
+//// 합집합도 거의 동일한 방식으로 개수를 구했다. 
+//
+//// 다른 친구와 얘기를 해보니 훨씬 효율적으로 풀어냈다. 
+//// 교집합의 개수를 구할때 교집합의 개수는 두개의 집합 중 더 원소의 개수가 적은 집합의 원소개수를 
+//// 넘을 수 없다는 아이디어를 적용하여 소거법으로 풀어냈다. 
+//// ex) 더 원소가 적은 집합 A, 그렇지않은집합 B일때, A의 원소를 for문으로 돌며 B에서 하나씩 소거하며 
+//// 같으면 개수를 증가시켰다.
+//// 그리고 합집합의 개수는 n(A) + n(B) - n(A ^ B)의 공식을 이용하여 풀어냈다. 
+//
+//// 코딩 전, 방법이 생각나도 한번 더 생각해보며 더 효율적인 방법을 찾으려는 노력이 필요해보인다.
+//
+//
+//void MakeMultipleSet(vector<string>& s,string str)
+//{
+//    for (int i = 0; i < str.length() - 1; i++)
+//    {
+//        // 두글자 모두 알파벳이면
+//        if (isalpha(str[i]) && isalpha(str[i + 1]))
+//        {
+//            string a;
+//            a += str[i];
+//            a += str[i + 1];
+//            s.push_back(a);
+//        }
+//    }
+//}
+//
+//
+//int solution(string str1, string str2) {
+//    int answer = 0;
+//
+//    vector<string> s1;
+//    vector<string> s2;
+//
+//    // 문자열의 대문자를 소문자로
+//    transform(str1.begin(), str1.end(), str1.begin(), ::tolower);
+//    transform(str2.begin(), str2.end(), str2.begin(), ::tolower);
+//
+//    //다중집합 만들기
+//    MakeMultipleSet(s1, str1);
+//    MakeMultipleSet(s2, str2);
+//
+//    if (s1.size() == 0 && s2.size()==0)return 65536;
+//
+//    // 오름차순 정렬
+//    sort(s1.begin(), s1.end());
+//    sort(s2.begin(), s2.end());
+//
+//    int id_s1 = 0;
+//    int id_s2 = 0;
+//    int cnt1 = 0, cnt2 = 0;
+//    int iCnt = 0;// 교집합 개수
+//    int SCnt = 0;// 합집합 개수
+//    
+//    // 교집합 개수 구하기
+//    while (id_s1 < s1.size() && id_s2 < s2.size())
+//    {
+//        // 같은 원소면
+//        if (s1[id_s1] == s2[id_s2])
+//        {
+//            int i = id_s1 + 1;
+//            while (i < s1.size()&&(s1[id_s1] == s1[i]))
+//            {
+//                i++;
+//            }
+//            cnt1 = i - id_s1;
+//            i = id_s2 + 1;
+//            while (i < s2.size()&&(s2[id_s2] == s2[i]))
+//            {
+//                i++;
+//            }
+//            cnt2 = i - id_s2;
+//            int minCnt = min(cnt1, cnt2);
+//            iCnt += minCnt;
+//            id_s1+=cnt1; id_s2+=cnt2;
+//        }
+//        else
+//        {
+//            if (s1[id_s1] < s2[id_s2])id_s1++;
+//            else id_s2++;
+//        }
+//    }
+//    cout << iCnt;
+//    id_s1 = 0; id_s2 = 0;
+//    // 합집합 개수 구하기
+//    while (id_s1 < s1.size() && id_s2 < s2.size())
+//    {
+//        // 같은 원소면
+//        if (s1[id_s1] == s2[id_s2])
+//        {
+//            int i = id_s1 + 1;
+//            while (i < s1.size() && (s1[id_s1] == s1[i]))i++;
+//            cnt1 = i - id_s1;
+//            i = id_s2 + 1;
+//            while (i < s2.size() && (s2[id_s2] == s2[i]))i++;
+//            cnt2 = i - id_s2;
+//            int maxCnt = max(cnt1, cnt2);
+//            SCnt += maxCnt;
+//            id_s1+=cnt1; id_s2+=cnt2;
+//        }
+//        else
+//        {
+//            if (s1[id_s1] < s2[id_s2])id_s1++;
+//            else id_s2++;
+//            SCnt++;
+//        }
+//    }
+//    // 남은 원소 
+//    // s1의 원소가 남았다면
+//    while (id_s1 < s1.size())
+//    {
+//        id_s1++; SCnt++;
+//    }
+//    // s2의 원소가 남았다면
+//    while (id_s2 < s2.size())
+//    {
+//        id_s2++; SCnt++;
+//    }
+//    cout << iCnt;
+//    cout << SCnt;
+//    double similarity = (double)iCnt / SCnt;
+//    answer = similarity * 65536;
+//    return answer;
+//}
+//
+//
+//int main()
+//{
+//    string a = "E=M*C^2";
+//    string b = "e=m*c^2";
+//    solution(a, b);
+//    return 0;
+//}
---
+++ algorithmPractice/algorithmPractice/순위검색_ver2.cpp
@@ -0,0 +1,118 @@
+//#include <string>
+//#include <vector>
+//#include<sstream>
+//#include<algorithm>
+//using namespace std;
+//
+//// string을 다루는 대신 앞글자만 따서 비교
+//
+//// 지원자를 표시하는 자료형
+//typedef struct {
+//    /*string language;
+//    string job;
+//    string career;
+//    string soulFood;*/
+//    vector<char> useInfo; // 0 = language, 1 = job, 2 = career, 3 = soulFood
+//    int scores;
+//}Applicant;
+//
+//bool compare(Applicant& a, Applicant& b)
+//{
+//    if (a.scores > b.scores)return true;
+//    else return false;
+//}
+//
+//// 받은 문자열을 분리하여 applicant에 저장
+//Applicant splitString(string info, char separator = ' ')
+//{
+//    Applicant applicant;
+//    std::istringstream iss(info);
+//    std::string str_buf;
+//    int i = 0;
+//    while (getline(iss, str_buf, separator)) {
+//        if (str_buf == "and")continue;
+//        if (i < 4)applicant.useInfo.push_back(str_buf[0]); i++;
+//    }
+//    applicant.scores = stoi(str_buf);
+//    return applicant;
+//}
+//
+//// info에서 받은 문자열을 나눠서 applicant자료형에 저장
+//void MakeApplicantsInfo(vector<Applicant>& applicants, vector<string>& info)
+//{
+//    for (string s : info)
+//    {
+//        applicants.push_back(splitString(s));
+//    }
+//}
+//
+//// query조건에 만족하는 지원자가 몇인지 반환
+//int CountPassedApplicant(vector<Applicant>& applicants, string query)
+//{
+//    int cnt = 0;
+//
+//    // query 분석
+//    Applicant tester = splitString(query);
+//
+//
+//    for (Applicant a : applicants)
+//    {
+//        if (a.scores < tester.scores)break;
+//        // 점수가 기준을 넘으면
+//        if (a.scores >= tester.scores)
+//        {
+//            //다른 조건 체크
+//            bool isSatisfied = true;
+//            for (int i = 0; i < 4; i++)
+//            {
+//                if (tester.useInfo[i] == '-')continue;
+//                if (a.useInfo[i] != tester.useInfo[i])
+//                {
+//                    isSatisfied = false; break;
+//                }
+//            }
+//            if (isSatisfied)cnt++;
+//        }
+//    }
+//    return cnt;
+//}
+//
+//vector<int> solution(vector<string> info, vector<string> query) {
+//    vector<int> answer;
+//    vector<Applicant>applicants;
+//
+//    MakeApplicantsInfo(applicants, info);
+//
+//    //스코어 기준으로 applicants 정렬
+//    sort(applicants.begin(), applicants.end(), compare);
+//
+//
+//    for (string s : query)
+//    {
+//        answer.push_back(CountPassedApplicant(applicants, s));
+//    }
+//
+//    return answer;
+//}
+//
+//int main()
+//{
+//    vector<string> b = {
+//        "java backend junior pizza 150",
+//        "python frontend senior chicken 210",
+//        "python frontend senior chicken 150",
+//        "cpp backend senior pizza 260",
+//        "java backend junior chicken 80",
+//        "python backend senior chicken 50"
+//    };
+//    vector<string> c = {
+//        "java and backend and junior and pizza 100",
+//        "python and frontend and senior and chicken 200",
+//        "cpp and - and senior and pizza 250",
+//        "- and backend and senior and - 150",
+//        "- and - and - and chicken 100",
+//        "- and - and - and - 150"
+//    };
+//    solution(b, c);
+//    return 0;
+//}
