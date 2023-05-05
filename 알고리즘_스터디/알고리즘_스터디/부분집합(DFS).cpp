cc21a6c57731fe9907d410af4f877fdc8c0a554d
gxtxhm <53137918+gxtxhm@users.noreply.github.com>
2023-05-06 02:01:20 +0900

    finish - 특정수만들기 - 인프런

--- 알고리즘_스터디/알고리즘_스터디/부분집합(DFS).cpp
+++ 알고리즘_스터디/알고리즘_스터디/부분집합(DFS).cpp
@@ -1,37 +1,37 @@
-#include<iostream>
-
-using namespace std;
-
-int num;
-int total;
-bool flag = false;
-int arr[11];
-
-void D(int n,int sum)
-{
-	if (flag == true)return;
-	if (n == num+1)
-	{
-		if (sum == (total - sum))flag = true;
-	}
-	else 
-	{
-		D(n + 1,sum+arr[n]);
-		D(n + 1,sum);
-	}
-}
-
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin >> num;
-	for (int i = 0; i < num; i++)
-	{
-		cin >> arr[i]; total += arr[i];
-	}
-	
-
-	D(1,0);
-	cout << (flag ? "YES" : "NO");
-	return 0;
-}
+//#include<iostream>
+//
+//using namespace std;
+//
+//int num;
+//int total;
+//bool flag = false;
+//int arr[11];
+//
+//void D(int n,int sum)
+//{
+//	if (flag == true)return;
+//	if (n == num+1)
+//	{
+//		if (sum == (total - sum))flag = true;
+//	}
+//	else 
+//	{
+//		D(n + 1,sum+arr[n]);
+//		D(n + 1,sum);
+//	}
+//}
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin >> num;
+//	for (int i = 0; i < num; i++)
+//	{
+//		cin >> arr[i]; total += arr[i];
+//	}
+//	
+//
+//	D(1,0);
+//	cout << (flag ? "YES" : "NO");
+//	return 0;
+//}
---
+++ 인프런알고리즘/특정수만들기/특정수만들기.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+int num, m;
+int arr[11];
+bool flag = false;
+int cnt = 0;
+
+void D(int n,int sum)
+{
+	if (n == num + 1)
+	{
+		if (sum == m)
+		{
+			flag = true;
+			cnt++;
+		}
+		
+	}
+	else
+	{
+		D(n + 1, sum + arr[n]);
+		D(n + 1, sum - arr[n]);
+		D(n + 1, sum);
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+
+	cin >> num >> m;
+	for (int i = 1; i <= num; i++)cin >> arr[i];
+
+	D(1, 0);
+	if (flag)cout << cnt;
+	else cout << -1;
+
+	return 0;
+}
